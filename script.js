5dfc63c8601790682ffa3d938b432d7c32c07827
basitali111 <allahbadshah148@gmail.com>
2022-06-24 23:23:55 +0500

    removing error in linter

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-let previewcontanier = document.querySelector(".popupcontainer");
-let previewbox = previewcontanier.querySelectorAll(".model");
-document.querySelectorAll(".works .workcard").forEach((workcard) => {
+const previewcontanier = document.querySelector('.popupcontainer');
+const previewbox = previewcontanier.querySelectorAll('.model');
+document.querySelectorAll('.works .workcard').forEach((workcard) => {
   workcard.onclick = () => {
-    previewcontanier.style.display = "flex";
-    let name = workcard.getAttribute("data-name");
+    previewcontanier.style.display = 'flex';
+    const name = workcard.getAttribute('data-name');
     previewbox.forEach((model) => {
-      let target = model.getAttribute("data-target");
-      if (name == target) {
-        model.classList.add("active1");
+      const target = model.getAttribute('data-target');
+      if (name === target) {
+        model.classList.add('active1');
       }
     });
   };
 });
-previewbox.forEach(materialicons =>{
-    materialicons.querySelector('.w3-xxxlarge').onclick = () =>{
-        materialicons.classList.remove('active1');
-        previewcontanier.style.display = 'none';  
-    }
+previewbox.forEach((materialicons) => {
+  materialicons.querySelector('.w3-xxxlarge').onclick = () => {
+    materialicons.classList.remove('active1');
+    previewcontanier.style.display = 'none';
+  };
 });
