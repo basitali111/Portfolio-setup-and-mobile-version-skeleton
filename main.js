62af10503480a12e7a3849be2ee9dd8fa99e3a40
basitali111 <allahbadshah148@gmail.com>
2022-06-25 05:52:35 +0500

    adding stles and removin linters

--- main.js
+++ main.js
@@ -370,3 +370,17 @@ popuparray.forEach((element, i) => {
   card.innerHTML += popupcard;
   popup.appendChild(card);
 });
+// validate form
+const form = document.forms[0];
+const email = document.getElementById('email');
+const error = document.getElementById('error');
+
+form.addEventListener('submit', (event) => {
+  const messageWhileError = [];
+  if (email.value.match(/[A-Z]/)) {
+    messageWhileError.push('Email should be written in lowercase letters.');
+    error.innerHTML = messageWhileError.join(', ');
+    event.preventDefault();
+    event.stopPropagation();
+  }
+});
